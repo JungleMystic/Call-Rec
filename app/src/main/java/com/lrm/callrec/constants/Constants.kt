e5e088bc9db2bb61e1669783b10b3d5448112d94
JungleMystic <locharla.rammohan@gmail.com>
2023-09-12 08:47:35 +0530

    Initial Commit v1.0.0

---
+++ app/src/main/java/com/lrm/callrec/constants/Constants.kt
@@ -0,0 +1,7 @@
+package com.lrm.callrec.constants
+
+const val TAG = "MyLogMessages"
+
+// Run-time Permission Codes
+const val RECORD_AUDIO_PERMISSION_CODE = 100
+const val WRITE_EXTERNAL_STORAGE_PERMISSION_CODE = 110
--- app/src/main/java/com/lrm/callrec/fragments/HomeFragment.kt
+++ app/src/main/java/com/lrm/callrec/fragments/HomeFragment.kt
@@ -1,18 +1,33 @@
 package com.lrm.callrec.fragments
 
+import android.Manifest
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.lrm.callrec.R
+import com.lrm.callrec.constants.RECORD_AUDIO_PERMISSION_CODE
+import com.lrm.callrec.constants.WRITE_EXTERNAL_STORAGE_PERMISSION_CODE
 import com.lrm.callrec.databinding.FragmentHomeBinding
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
+import kotlin.random.Random
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private var recorder: MediaRecorder? = null
+    val mediaPlayer = MediaPlayer()
+    val outputPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/CallRec/myRec${Random.nextInt()}.3gp"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +46,104 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.startRec.setOnClickListener { startRecording() }
+        binding.stopRec.setOnClickListener { stopRecording() }
+        binding.playRec.setOnClickListener { playRecording() }
+        binding.stopPlay.setOnClickListener { stopPlaying() }
+    }
+
+    private fun stopPlaying() {
+        mediaPlayer.stop()
+    }
+
+    private fun playRecording() {
+        mediaPlayer.apply {
+            setDataSource(outputPath)
+            prepare()
+            start()
+        }
+    }
+
+
+    private fun startRecording() {
+        if (!hasRecordAudioPermission()) {
+            requestRecordAudioPermission()
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            if (!hasWriteExternalStoragePermission()) {
+                requestWriteExternalStoragePermission()
+                return
+            }
+        }
+
+        createRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(outputPath)
+            prepare()
+            start()
+            recorder = this
+        }
+
+        Toast.makeText(requireContext(), "Recording Audio...", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun stopRecording() {
+        recorder?.stop()
+        recorder?.reset()
+        recorder = null
+        Toast.makeText(requireContext(), "Recording stopped...", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createRecorder(): MediaRecorder {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MediaRecorder(requireContext())
+        } else MediaRecorder()
+    }
+
+    private fun hasRecordAudioPermission(): Boolean =
+        EasyPermissions.hasPermissions(requireContext(), Manifest.permission.RECORD_AUDIO)
+
+    private fun requestRecordAudioPermission() {
+        EasyPermissions.requestPermissions(
+            this,
+            "Permission is required to record audio",
+            RECORD_AUDIO_PERMISSION_CODE,
+            Manifest.permission.RECORD_AUDIO
+        )
+    }
+
+    private fun hasWriteExternalStoragePermission(): Boolean =
+        EasyPermissions.hasPermissions(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    private fun requestWriteExternalStoragePermission() {
+        EasyPermissions.requestPermissions(
+            this,
+            "Permission is required to record audio",
+            WRITE_EXTERNAL_STORAGE_PERMISSION_CODE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.permissionPermanentlyDenied(this, perms.first())) {
+            SettingsDialog.Builder(requireContext()).build().show()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Toast.makeText(requireContext(), "Permission Granted", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
