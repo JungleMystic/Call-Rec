ff9ab6b6bb0111a9bbda8433e6361394b757e4c9
JungleMystic <locharla.rammohan@gmail.com>
2023-09-14 09:00:04 +0530

    Initial Commit v1.0.0

--- app/src/main/java/com/lrm/callrec/constants/Constants.kt
+++ app/src/main/java/com/lrm/callrec/constants/Constants.kt
@@ -4,4 +4,5 @@ const val TAG = "MyLogMessages"
 
 // Run-time Permission Codes
 const val RECORD_AUDIO_PERMISSION_CODE = 100
-const val WRITE_EXTERNAL_STORAGE_PERMISSION_CODE = 110
+const val READ_AUDIO_PERMISSION_CODE = 110
+const val WRITE_EXTERNAL_STORAGE_PERMISSION_CODE = 120
--- app/src/main/java/com/lrm/callrec/fragments/HomeFragment.kt
+++ app/src/main/java/com/lrm/callrec/fragments/HomeFragment.kt
@@ -1,14 +1,8 @@
 package com.lrm.callrec.fragments
 
 import android.Manifest
-import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.provider.Settings.SettingNotFoundException
-import android.text.TextUtils.SimpleStringSplitter
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,11 +11,10 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.lrm.callrec.R
+import com.lrm.callrec.constants.READ_AUDIO_PERMISSION_CODE
 import com.lrm.callrec.constants.RECORD_AUDIO_PERMISSION_CODE
-import com.lrm.callrec.constants.TAG
 import com.lrm.callrec.constants.WRITE_EXTERNAL_STORAGE_PERMISSION_CODE
 import com.lrm.callrec.databinding.FragmentHomeBinding
-import com.lrm.callrec.utils.CallRecord
 import com.vmadalin.easypermissions.EasyPermissions
 import com.vmadalin.easypermissions.dialogs.SettingsDialog
 
@@ -29,8 +22,6 @@ class HomeFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var callRecord: CallRecord
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,38 +40,20 @@ class HomeFragment : Fragment(), EasyPermissions.PermissionCallbacks {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        callRecord = CallRecord(requireContext())
+        if (!checkPermissions()) {
+            showPermissionRequiredDialog()
+            requestPermissionsRequired()
+        }
 
-        binding.startRec.setOnClickListener { startRecording() }
-        binding.stopRec.setOnClickListener { stopRecording() }
-        binding.playRec.setOnClickListener { callRecord.playRecording() }
-        binding.stopPlay.setOnClickListener { callRecord.stopPlaying() }
     }
 
     private fun startRecording() {
-        if (!hasRecordAudioPermission()) {
-            requestRecordAudioPermission()
-            return
-        }
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-            if (!hasWriteExternalStoragePermission()) {
-                requestWriteExternalStoragePermission()
-                return
-            }
-        }
-
-        if (!isAccessibilityEnabled(requireContext())) {
-            showTurnOnAccessibilityDialog()
-            return
-        }
-
-        callRecord.startRecording()
+        checkPermissions()
 
         Toast.makeText(requireContext(), "Recording Audio...", Toast.LENGTH_SHORT).show()
     }
 
     private fun stopRecording() {
-        callRecord.stopRecording()
         Toast.makeText(requireContext(), "Recording stopped...", Toast.LENGTH_SHORT).show()
     }
 
@@ -94,6 +67,33 @@ class HomeFragment : Fragment(), EasyPermissions.PermissionCallbacks {
             RECORD_AUDIO_PERMISSION_CODE,
             Manifest.permission.RECORD_AUDIO
         )
+    }
+
+    private fun hasReadAudioPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            EasyPermissions.hasPermissions(requireContext(), Manifest.permission.READ_MEDIA_AUDIO)
+        } else {
+            EasyPermissions.hasPermissions(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+
+    private fun requestReadAudioPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            EasyPermissions.requestPermissions(
+                this,
+                "Permission is required to record audio",
+                READ_AUDIO_PERMISSION_CODE,
+                Manifest.permission.READ_MEDIA_AUDIO
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "Permission is required to record audio",
+                READ_AUDIO_PERMISSION_CODE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
     }
 
     private fun hasWriteExternalStoragePermission(): Boolean =
@@ -125,55 +125,53 @@ class HomeFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
         Toast.makeText(requireContext(), "Permission Granted", Toast.LENGTH_SHORT).show()
+        if (!checkPermissions()) requestPermissionsRequired()
     }
 
-    private fun showTurnOnAccessibilityDialog() {
+    private fun showPermissionRequiredDialog() {
         MaterialAlertDialogBuilder(requireContext())
-            .setTitle("Need to Turn on Accessibility Service")
+            .setTitle("Permissions Request")
+            .setMessage("This app requires some permissions, So please allow them.")
             .setCancelable(true)
-            .setPositiveButton("Go to Settings") { _, _ ->
-                val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
-                startActivity(intent)
+            .setPositiveButton("Ok") { _, _ ->
+                requestPermissionsRequired()
             }
             .show()
     }
 
-    private fun isAccessibilityEnabled(context: Context): Boolean {
-        var accessibilityEnabled = 0
-        val service: String = context.packageName + "/com.lrm.callrec.services.CallRecordService"
-        Log.i(TAG, "isAccessibilityEnabled: our service -> $service")
-        try {
-            accessibilityEnabled = Settings.Secure.getInt(
-                context.applicationContext.contentResolver,
-                Settings.Secure.ACCESSIBILITY_ENABLED
-            )
-            Log.i(TAG, "isAccessibilityEnabled: $accessibilityEnabled")
-        } catch (e: SettingNotFoundException) {
-            Log.i(TAG, "Error finding setting, default accessibility to not found: " + e.message)
+    private fun checkPermissions(): Boolean {
+        if (!hasRecordAudioPermission()) {
+            return false
         }
 
-        val colonSplit = SimpleStringSplitter(':')
-        if (accessibilityEnabled == 1) {
-            Log.i(TAG, "isAccessibilityEnabled is enabled")
-            val settingValue: String = Settings.Secure.getString(
-                context.applicationContext.contentResolver,
-                Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
-            )
-            if (settingValue != null) {
-                colonSplit.setString(settingValue)
-                while (colonSplit.hasNext()) {
-                    val accessibilityService = colonSplit.next()
-                    Log.i(TAG, "AccessibilityService :: \n $accessibilityService \n $service")
-                    if (accessibilityService.equals(service, ignoreCase = true)) {
-                        Log.i(TAG, "accessibility is switched on!")
-                        return true
-                    }
-                }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            if (!hasWriteExternalStoragePermission()) {
+                return false
             }
-        } else {
-            Log.i(TAG, "accessibility is disabled")
         }
-        return false
+
+        if (!hasReadAudioPermission()) {
+            return false
+        }
+
+        return true
+    }
+
+    private fun requestPermissionsRequired() {
+        if (!hasRecordAudioPermission()) {
+            requestRecordAudioPermission()
+            return
+        }
+
+        if (!hasWriteExternalStoragePermission()) {
+            requestWriteExternalStoragePermission()
+            return
+        }
+
+        if (!hasReadAudioPermission()) {
+            requestReadAudioPermission()
+            return
+        }
     }
 
     override fun onDestroyView() {
